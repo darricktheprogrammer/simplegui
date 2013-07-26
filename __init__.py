0a48c26990c5889481e0c6ecfefeadf094035430
Darrick Herwehe <darrick@exitcodeone.com>
2013-07-26 10:27:00 -0400

    Revert "Initial commit"
    This reverts commit 2b6509d296e810bd88b5f1dc337bb2497da6c35b.

--- __init__.py
+++
@@ -1,3 +0,0 @@
-'''A Library for easier creation of simple dialogs'''
-
-import simplegui
--- config.py
+++
@@ -1,8 +0,0 @@
-'''Configuration settings for simplegui'''
-
-BG_COLOR = "#EDEDED"
-BG_SHADOW = "#888888"
-PADDING_TOP = 0
-PADDING_BOTTOM = 10
-PADDING_CENTER = 10
-PADDING_OUTER = 30
